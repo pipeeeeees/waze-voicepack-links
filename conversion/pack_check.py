a9fa20faa68d30d7d499fb45fc02bec48e316e80
pipeeeeees <48690614+pipeeeeees@users.noreply.github.com>
2023-04-01 12:27:10 -0400

    adding .mp3 checking and formatting

---
+++ conversion/pack_check.py
@@ -0,0 +1,11 @@
+
+
+"""
+checks directories in `input_packs/`, and will check if files are present, formatted correctly, and return a valid output pack
+
+Bitrate: Constant 64 kbps
+Channels/Modus: Mono
+Sample frequency: 44100Hz
+
+"""
+
